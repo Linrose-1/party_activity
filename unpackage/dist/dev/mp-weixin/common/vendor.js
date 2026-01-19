1b89ee7316a8824a4789c954b1d01a6a3c3ba47a
Linrose-1 <3036359770@qq.com>
2026-01-19 12:58:30 +0800

    1

--- unpackage/dist/dev/mp-weixin/common/vendor.js
+++ unpackage/dist/dev/mp-weixin/common/vendor.js
@@ -7299,7 +7299,7 @@ function initOnError() {
 function initRuntimeSocketService() {
   const hosts = "172.20.10.4,127.0.0.1";
   const port = "8090";
-  const id = "mp-weixin_-NfGGv";
+  const id = "mp-weixin_i_0gtt";
   const lazy = typeof swan !== "undefined";
   let restoreError = lazy ? () => {
   } : initOnError();
