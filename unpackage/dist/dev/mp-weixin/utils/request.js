b0991ff1ca7f19f2eae264ae1ffb6b092ff2edf6
Linrose-1 <3036359770@qq.com>
2026-03-03 23:42:36 +0800

    26.3.3

--- unpackage/dist/dev/mp-weixin/utils/request.js
+++ unpackage/dist/dev/mp-weixin/utils/request.js
@@ -1,6 +1,6 @@
 "use strict";
 const common_vendor = require("../common/vendor.js");
-const baseURL = "https://test-api.gofor.club";
+const baseURL = "https://jyj.gofor.club";
 const timeout = 1e4;
 const request = async (url, options = {}) => {
   const headers = {
